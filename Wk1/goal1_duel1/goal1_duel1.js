86cc5cd80fd8559aea38637b0d19b69877a6d58b
richie17931 <richardarrigo@Richards-MacBook-Pro-9.local>
2013-11-26 23:20:35 -0500

    goal1_duel_CommitThree

--- Wk1/goal1_duel1/goal1_duel1.js
+++ Wk1/goal1_duel1/goal1_duel1.js
@@ -4,24 +4,35 @@
 
 // Richie Arrigo | 2492145 | 11/26/13 | Goal 1: Duel 1
 
+(function(){
 
-// variables for player names
-var playerOne=prompt('Enter Player One\'s name: ');
-var playerTwo=prompt('Enter Player Two\'s name: ');
-// variables to store players' health
-var playerOneHealth= 100;
-var playerTwoHealth= 100;
-// variables for player damage
-var playerOneDamage= 50;
-var playerTwoDamage= 50;
+    // variables for player names
+    var playerOne=prompt('Enter Player One\'s name: ');
+    var playerTwo=prompt('Enter Player Two\'s name: ');
+    // variables to store players' health
+    var playerOneHealth= 100;
+    var playerTwoHealth= 100;
+    // variables for player damage
+    var playerOneDamage= 25;
+    var playerTwoDamage= 25;
+    // variable to store the round number;
+    var round= 1;
 
-// fight() function to loop through rounds
-function fight(p1Health, p2Health, p1Damage, p2Damage){
 
-}
+    // fight() function to loop through rounds
+    function fight(){
 
-// winnerCheck() function to determine if there is a winner
-function winnerCheck(){
 
-}
+        winnerCheck();
+        round++;
+    };
+
+    // winnerCheck() function to determine if there is a winner
+    function winnerCheck(){
+
+    };
+
+    fight();
+
+})();
 
