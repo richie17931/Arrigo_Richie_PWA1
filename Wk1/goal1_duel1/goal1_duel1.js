0607973bccf5fbb1fc89034571ca0e6f5e6a3cda
richie17931 <richardarrigo@Richards-MacBook-Pro-9.local>
2013-11-26 23:12:28 -0500

    goal1_duel1_CommitTwo

--- Wk1/goal1_duel1/goal1_duel1.js
+++ Wk1/goal1_duel1/goal1_duel1.js
@@ -2,6 +2,9 @@
  * Created by richardarrigo on 11/26/13.
  */
 
+// Richie Arrigo | 2492145 | 11/26/13 | Goal 1: Duel 1
+
+
 // variables for player names
 var playerOne=prompt('Enter Player One\'s name: ');
 var playerTwo=prompt('Enter Player Two\'s name: ');
@@ -11,4 +14,14 @@ var playerTwoHealth= 100;
 // variables for player damage
 var playerOneDamage= 50;
 var playerTwoDamage= 50;
+
+// fight() function to loop through rounds
+function fight(p1Health, p2Health, p1Damage, p2Damage){
+
+}
+
+// winnerCheck() function to determine if there is a winner
+function winnerCheck(){
+
+}
 
