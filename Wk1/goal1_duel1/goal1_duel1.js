f7a745bcf7a57941ec4e10faec64b007b145153f
richie17931 <richardarrigo@Richards-MacBook-Pro-9.local>
2013-11-26 23:05:17 -0500

    Initial Commit

---
+++ Wk1/goal1_duel1/goal1_duel1.js
@@ -0,0 +1,14 @@
+/**
+ * Created by richardarrigo on 11/26/13.
+ */
+
+// variables for player names
+var playerOne=prompt('Enter Player One\'s name: ');
+var playerTwo=prompt('Enter Player Two\'s name: ');
+// variables to store players' health
+var playerOneHealth= 100;
+var playerTwoHealth= 100;
+// variables for player damage
+var playerOneDamage= 50;
+var playerTwoDamage= 50;
+
