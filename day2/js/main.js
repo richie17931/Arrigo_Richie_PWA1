16b9f83cdfa7a097e8e22ed98e9e7f8559682bce
richie17931 <richardarrigo@Richards-MacBook-Pro-9.local>
2013-12-03 20:29:32 -0500

    Day2SecondCommit

--- day2/js/main.js
+++ day2/js/main.js
@@ -2,20 +2,36 @@
  * Created by richardarrigo on 12/3/13.
  */
 
-var test = "Hello";
+//var test = "Hello";
+//
+//var firstLetter = test.charAt(0); // H
+//var partialString = test.substr(1,3); // ell
+////test.length //5
+//
+//function coolDate(){
+//    var days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+//    var date = new Date();
+//    var month = date.getMonth()+1;
+//    var day = date.getDate();
+//    var year = date.getFullYear();
+//    var dayName = days[date.getDay()];
+//    return dayName + ', ' + month + '/' + day + '/' + year;
+//}
+//
+//console.log(coolDate());
 
-var firstLetter = test.charAt(0); // H
-var partialString = test.substr(1,3); // ell
-//test.length //5
-
-function coolDate(){
-    var days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-    var date = new Date();
-    var month = date.getMonth()+1;
-    var day = date.getDate();
-    var year = date.getFullYear();
-    var dayName = days[date.getDay()];
-    return dayName + ', ' + month + '/' + day + '/' + year;
+var deck = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q"];
+function deal(){
+    var cardNumber = ~~(Math.random()*deck.length);
+    var card = deck[cardNumber];
+    deck.splice(cardNumber,1);
+    return card;
 }
 
-console.log(coolDate());
+function play(){
+    for(i=0;i<13;i++){
+        console.log(i+1+":", deal(), deck);
+    }
+}
+
+play();
