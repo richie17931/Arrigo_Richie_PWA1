6f648bee4464fba23559fdee6a71e8345ad99f66
richie17931 <richardarrigo@Richards-MacBook-Pro-9.local>
2013-12-14 20:17:05 -0500

    Day8FinalCommit
    lecture

--- day8project/js/main.js
+++ day8project/js/main.js
@@ -14,22 +14,38 @@
                button: document.querySelector("#next")};
 
     dom.button.addEventListener("click", onClick);
+    makeBooks(3);
+    console.log(books);
+    updateHTML();
 
     function makeBooks(n){
-        var bookNum = ~~(Math.random()*bookNames.length);
-        var authorNum = ~~(Math.random()*authors.length);
         for(var i = 0; i < n; i++){
+            var bookNum = ~~(Math.random()*bookNames.length);
+            var authorNum = ~~(Math.random()*authors.length);
             var book = new Book();
             book.name = bookNames[bookNum];
+            bookNames.splice(bookNum,1);
             book.author = authors[authorNum];
+            authors.splice(authorNum,1);
             book.isbn = Math.random();
             book.price = ~~(Math.random()*51+50);
+            books.push(book);
         }
     }
 
-    function onClick(e){
+    function updateHTML(){
+        dom.name.innerHTML = books[count].name;
+        dom.isbn.innerHTML = books[count].isbn;
+        dom.author.innerHTML = books[count].author;
+        dom.price.innerHTML = books[count].price;
+    }
 
+    function onClick(e){
         count++
+        if(count > books.length-1){
+            count = 0;
+        }
+        updateHTML();
     }
 
 
